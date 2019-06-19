07c8df9599f8b54c756e19bfb8964d7e8204f7ae
pbejuga <pbejuga@gap.com>
2019-06-19 13:54:02 +0530

    commit

---
+++ src/main/java/com/nisum/tanuj/jenkins_trial/App.java
@@ -0,0 +1,13 @@
+package com.nisum.tanuj.jenkins_trial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
